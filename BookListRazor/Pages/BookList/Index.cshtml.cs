38422131ea0c7041e9a0328e5bda92240e2b562a
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-12-24 11:50:17 +0000

    Created new Page BookList to display the list of books.

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public IEnumerable<Book> Books { get; set; }
+        public IndexModel(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task OnGet()
+        {
+            Books = await _dbContext.Books.ToListAsync();
+        }
+
+    }
+}
